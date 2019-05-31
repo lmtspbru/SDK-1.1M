2101af7997311c29fffba3e3fd28f2c100b5a6e8
vakluchev <35079397+vakluchev@users.noreply.github.com>
2019-05-31 13:56:04 +0300

    LED lab

---
+++ SDK_LED/Debug/Core/Src/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/gpio.c \
+../Core/Src/main.c \
+../Core/Src/stm32f4xx_hal_msp.c \
+../Core/Src/stm32f4xx_it.c \
+../Core/Src/system_stm32f4xx.c 
+
+OBJS += \
+./Core/Src/gpio.o \
+./Core/Src/main.o \
+./Core/Src/stm32f4xx_hal_msp.o \
+./Core/Src/stm32f4xx_it.o \
+./Core/Src/system_stm32f4xx.o 
+
+C_DEPS += \
+./Core/Src/gpio.d \
+./Core/Src/main.d \
+./Core/Src/stm32f4xx_hal_msp.d \
+./Core/Src/stm32f4xx_it.d \
+./Core/Src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o: ../Core/Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F407xx -I"C:/SDK11M/SDK_Project/SDK_LED/Core/Inc" -I"C:/SDK11M/SDK_Project/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc" -I"C:/SDK11M/SDK_Project/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"C:/SDK11M/SDK_Project/SDK_LED/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/SDK11M/SDK_Project/SDK_LED/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
