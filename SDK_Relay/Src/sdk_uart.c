1cec9dd1e1d97ef10bbbf5f5ce1d89f694ba7a1c
vakluchev <vakluchev@gmail.com>
2019-09-25 15:47:10 +0300

    Add Relay example

---
+++ SDK_Relay/Src/sdk_uart.c
@@ -0,0 +1,16 @@
+//===================================================================//
+//								USART6								 //
+//===================================================================//
+#include "sdk_uart.h"
+#include "usart.h"
+
+
+void UART_Transmit(uint8_t data[]){
+	HAL_UART_Transmit(&huart6, data, strlen(data), 1);
+}
+
+HAL_StatusTypeDef UART_Receive(uint8_t *data, uint8_t len){
+	HAL_StatusTypeDef st = HAL_ERROR;
+	st = HAL_UART_Receive(&huart6, data, len, 0xFFFF);
+	return st;
+}
